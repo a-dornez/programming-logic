a5f2ba4c895f122220046206627f86acf83729a5
a-dornez <142704596+a-dornez@users.noreply.github.com>
2023-09-11 18:09:39 -0300

    feature: added 'PaymentWorkedHours' class
    calculates payment based on inputed houtly rate and total worked hours of a service provider

---
+++ Java/src/LandAreaAndPrice/LandAreaAndPrice.java
@@ -0,0 +1,38 @@
+/* A programm that receives a land's length, width, square meter's price ans then
+ * prints land's area and total price. Problem given by Nelio Alves.
+ */
+
+package LandAreaAndPrice;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class LandAreaAndPrice {
+
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Greetings!");
+
+        //Colecting user's inputs       
+        System.out.println("Enter the land's length: ");
+         double landsLength = scanner.nextDouble();
+        System.out.println("enter the land's width: ");
+         double landsWidth = scanner.nextDouble();
+        System.out.println("Enter the square meter's price: ");
+         double squareMeterPrice = scanner.nextDouble();
+
+        //calculating land's total area
+        double landsTotalArea = landsWidth * landsLength;
+
+        //calculating land's total price
+        double landsTotalPrice = squareMeterPrice * landsTotalArea;
+
+        //Printing processed inputs to inform total price and total area
+        System.out.println("\nThe land's total area is " + String.format("%.2f", landsTotalArea) +
+         " m2, and it's total price is $" + String.format("%.2f", landsTotalPrice) + ".\n");
+
+        scanner.close();
+    }
+}
---
+++ Java/src/PaymentWorkedHours/PaymentWorkedHours.java
@@ -0,0 +1,38 @@
+/* "Create a program that calculates a service provider's payment. 
+ * That programm must the user to input the provider's name, hourly rate
+ * and worked hours. And then print the payment's value."
+ */
+
+package PaymentWorkedHours;
+
+import java.util.Locale;
+
+import java.util.Scanner;
+
+public class PaymentWorkedHours{
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nWelcome!\n");
+
+        //Asks and collects user inputs
+        System.out.println("Insert the service provider's name: ");
+         String providersName = scanner.nextLine();
+        System.out.println("Insert the provider's hourly rate: ");
+         double hourlyRate = scanner.nextDouble();
+        System.out.println("Insert the provider's total worked hours: ");
+         double workedHours = scanner.nextDouble();
+
+        //calculates payment
+        double payment = hourlyRate * workedHours;
+        
+        //Displays payment value
+        System.out.println("\nThe payment for " + providersName + " is $" + String.format("%.2f", payment) +  ".\n" );
+
+        scanner.close();
+    }
+
+}
