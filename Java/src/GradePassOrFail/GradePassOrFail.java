f437d94284c16fee3d2513c6403f5d1521846f38
a-dornez <142704596+a-dornez@users.noreply.github.com>
2023-09-13 01:23:54 -0300

    fix: loop asking if user wants to run programm again corrected
    refactor: replaced magical number for constant 'MINUMUM_PASSING_GRADE'

--- Java/src/GradePassOrFail/GradePassOrFail.java
+++ Java/src/GradePassOrFail/GradePassOrFail.java
@@ -17,21 +17,32 @@
 
         System.out.println("\nSalutations!\n");
 
-        //Asks and collects user inputs
-        System.out.println("Please, insert first semester's grade: ");
-         double firstSemesterGrade = scanner.nextDouble();
-        System.out.println("Now, insert th seond semester's grade: ");
-         double secondSemesterGrade = scanner.nextDouble();
-        
-        double finalGrade = firstSemesterGrade +  secondSemesterGrade;
+        String verifyAnother;
+        do { 
+            //Asks and collects user inputs
+            System.out.println("Please, insert first semester's grade: ");
+            double firstSemesterGrade = scanner.nextDouble();
+            System.out.println("Now, insert th second semester's grade: ");
+            double secondSemesterGrade = scanner.nextDouble();
+            
+            //calculates final grade
+            double finalGrade = firstSemesterGrade +  secondSemesterGrade;
 
-        System.out.println("\nThe final grade is " + String.format("%.1f", finalGrade) + ".");
+            //prints final grade
+            System.out.println("\nThe final grade is " + String.format("%.1f", finalGrade) + ".");
 
-        if ( finalGrade >= 60 )
-            System.out.println("\nPASS. Final grade meets the requirements.\n");
-        else
-            System.out.println("\nFAIL. Final grade is under minimum required.\n");
-        
-        scanner.close();
+            final int MINIMUM_PASSING_GRADE = 60;
+            if ( finalGrade >= MINIMUM_PASSING_GRADE )
+                System.out.println("\nPASS. Final grade meets the requirements.\n");
+            else
+                System.out.println("\nFAIL. Final grade is under minimum required.\n");
+            
+            System.out.println("Do you wish to verify another student's status?  ");
+             scanner.nextLine();
+             verifyAnother = scanner.nextLine();
+
+        }while( verifyAnother.equalsIgnoreCase("y"));
+
+         scanner.close();
     }
  }
