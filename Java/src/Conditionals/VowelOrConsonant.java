e83bbae93ead532e4dd54e6ca6c69816039a8a70
a-dornez <142704596+a-dornez@users.noreply.github>
2023-11-13 23:41:48 -0300

    feature: added 'VowelOrConsonant' verifies if an entered letter is either a vowel or a consonant

---
+++ Java/src/Conditionals/VowelOrConsonant.java
@@ -0,0 +1,31 @@
+/*
+ * "Write a program that verifies if an entered letter is either a vowel or a consonant"
+ *  --problem at www.computersciencemaster.com/exercicios-if-else/
+ */
+
+package Conditionals;
+
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+	
+		public static void main (String[] args) {
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.print("Please,insert a letter: ");
+			 char letter = scanner.nextLine().charAt(0);
+			 
+			if (letter == 'a'||letter == 'A' || letter == 'e'||letter == 'E'|| letter == 'i'|| letter == 'I'
+					|| letter == 'o'||letter == 'O' || letter == 'u'||letter == 'U') {
+				
+				System.out.println(letter +" is a vowel.");
+			}
+			else {
+				
+				System.out.println(letter + " is a consonant");
+			}
+			
+			scanner.close();
+		}
+}
