bb14b0b12cbcca6aebd54e7b6fd8b76aa579b1bf
a-dornez <142704596+a-dornez@users.noreply.github>
2023-11-13 21:53:19 -0300

    feature: added 'positiveOrNegativeNumber'
    reads an input and prints its polarity

---
+++ Java/src/Conditionals/PositiveOrNegative.java
@@ -0,0 +1,30 @@
+/*
+ * "Write a program that asks and reads a numerical value and prints if it is negative or positive"
+ *  --Problem at www.computersciencemaster.com/exercicios-if-else/
+ */
+
+package Conditionals;
+
+import java.util.Scanner;
+
+public class PositiveOrNegative {
+	
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//asks and collects input
+		System.out.print("Please, insert a positive or negative number: ");
+		 double positiveOrNegativeNumber = scanner.nextDouble();
+		
+		//evaluates polarity 
+		if ( positiveOrNegativeNumber >= 0 ) {
+			System.out.println( positiveOrNegativeNumber + " is a positive number");
+		}
+		else {
+			System.out.println( positiveOrNegativeNumber + " is a negative number");
+		}
+		
+		scanner.close();
+	}
+}
