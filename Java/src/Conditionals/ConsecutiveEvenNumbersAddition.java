ee3c05f427189d17557931b10dfd442c331fbbd0
a-dornez <142704596+a-dornez@users.noreply.github.com>
2023-11-18 12:59:57 -0300

    feature: added 'ConsecutiveEvenNumbers' reads an 'x' int value and prints the sum of the five consecutive even numbers

---
+++ Java/src/Conditionals/ConsecutiveEvenNumbersAddition.java
@@ -0,0 +1,39 @@
+/*
+ * "Make program that reads a int value 'x' an indeterminate number of times (stops if value is 0).
+ * For each 'x' value read, print the sum of it's five consecutive even numbers,including 'x' itself
+ * if it's even."
+ *   -- problem by Nelio Alves 
+ */
+
+
+package Conditionals;
+
+import java.util.Scanner;
+
+public class ConsecutiveEvenNumbersAddition {
+	
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Please, insert an int value: ");
+		 int x = scanner.nextInt();
+		  
+		int evenConsecutivesSum = 0;  
+		while ( x != 0 ) {
+			if (x % 2 == 0) {
+				evenConsecutivesSum = 5 * x + 20;
+			} else {
+				x++;
+				evenConsecutivesSum = 5 * x + 20;
+			}	
+			
+			System.out.println("The sum of the five consecutive even numbers is: " + evenConsecutivesSum);
+			
+			System.out.println("\nPlease, insert an int value: ");
+			 x = scanner.nextInt();
+		}
+		
+		scanner.close();
+	}
+}
