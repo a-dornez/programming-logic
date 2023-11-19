d30669e01d3a44d9bb947d3271e425a3a800204f
a-dornez <142704596+a-dornez@users.noreply.github.com>
2023-11-18 21:42:35 -0300

    feature: added 'OddNumbersAddition'
    reads two int values and calculates the sum of the odd numbers between them

---
+++ Java/src/Loops/OddNumbersAddition.java
@@ -0,0 +1,43 @@
+/*
+ * "Make a program that reads two int values X and Y, in any order. Then, calculate and display
+ * the sum of the odd numbers between X and Y."
+ *   -- problem by Nelio Alves
+ */
+
+package Loops;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class OddNumbersAddition {
+	
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.print("Please insert a int number X: ");
+		 int numberX = scanner.nextInt();
+		System.out.print("Please insert a int number Y: ");
+		 int numberY = scanner.nextInt();
+		
+		int placeholder = 0;
+		//Ensures x and y are in ascending order
+		if ( numberY < numberX ) {
+			placeholder = numberX;
+			numberX = numberY;
+			numberY = placeholder;
+		}
+		
+		int oddNumbersTotalSum = 0;
+		for ( int i = numberX+1; i < numberY; i ++ ) {
+			//checks odd values
+			if ( i % 2 != 0) {
+				oddNumbersTotalSum = oddNumbersTotalSum + i;
+			}
+		}
+		
+		System.out.println("\nThe sum of all odd number between " + numberX +  " and " + numberY + " is: " + oddNumbersTotalSum + ".\n" );
+		
+		scanner.close();
+	}
+}
