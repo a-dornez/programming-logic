9804a2b1b7ecb545e47012cfb2531e49050d6d94
a-dornez <142704596+a-dornez@users.noreply.github.com>
2023-11-18 22:36:29 -0300

    feature: added 'InOutInterval'
    reads an amout of int inputs determined by user, then dispaly how many of them are inside or out an pre-defined interval

---
+++ Java/src/Loops/InOutInterval.java
@@ -0,0 +1,41 @@
+/*
+ * "Read a int value 'N'. This value will be the quantity of times that int values 'x' will be read.
+ * Then, display how many of those values were inside and out the interval [10,20]."
+ *  -- Problem by Nelio Alves
+ */
+
+package Loops;
+
+import java.util.Scanner;
+
+public class InOutInterval {
+	
+	public static void main (String [] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("How many int values you'll insert?: ");
+		 int inputAmount = scanner.nextInt();
+		
+		int in = 0;
+		int out = 0;
+		for ( int i = 1; i <= inputAmount; i++ ) {
+			
+			System.out.print("Insert an int value: ");
+			 int numberX = scanner.nextInt();
+			
+			final int MINIMUM_VALUE = 10;
+			final int MAXIMUM_VALUE = 20;
+			 
+			if ( numberX < MINIMUM_VALUE || numberX > MAXIMUM_VALUE ) {
+				out++;
+			} else {
+				in++;
+			}
+		}
+		
+		System.out.println("\n" + in + " In, " + out + " Out." );
+		
+		scanner.close();
+	}
+}
