ddceb58c365d04a0742f05fa861422271be3213b
a-dornez <142704596+a-dornez@users.noreply.github.com>
2024-03-26 16:29:13 -0300

    feature: added cpp solution for 'Ascending or descending'
    reads two numbers and prints if they are in ascending or descending order until two equal values are input

---
+++ cpp/loops/AscendingOrDescending.cpp
@@ -0,0 +1,34 @@
+/* "Read an undetermined amount of int values X and Y. Print for each pair
+* wether the were inserted in ascending or descending order. If the values
+* are equal, termiante execution."
+*   Problem by Nelio Alves
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int first, second;
+
+    while ( first != second ) {
+
+        cout << "Enter a integer value x: ";
+        cin >> first;
+        cout<< "Enter other integer value: ";
+        cin >> second;
+
+        if ( first < second ) {
+            cout << "\nX and y are in ascending order\n" << endl;
+        }
+        else if ( first > second) {
+            cout << "\nX and Y are in descending order\n" << endl;
+        }
+        else {
+            cout << "\nX and Y are equal values. Terminating execution\n" << endl;
+        }
+    }
+
+    return 0;
+}
